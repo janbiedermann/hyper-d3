5bd04082d1385582eef3ce02914a65ceba45b5db
Jan Biedermann <jan@kursator.de>
2018-02-14 19:55:33 +0100

    remove eval

--- lib/opal/d3/color_schemes.rb
+++ lib/opal/d3/color_schemes.rb
@@ -4,42 +4,31 @@ module D3
   class << self
     %w[Category10 Category20 Category20b Category20c
        Accent Dark2 Paired Pastel1 Pastel2 Set1 Set2 Set3].each do |native_category|
-      
-      eval <<-EOF
-        def scheme_#{native_category.underscore.sub(/[123]/,'_\0')}
-          `window.d3.scheme#{native_category}`
-        end
-      EOF
+      define_method("scheme_#{native_category.underscore.sub(/[123]/,'_\0')}") do
+        `window.d3`.JS["scheme#{native_category}"]
+      end
     end
 
     %w[Blues Greens Greys Oranges Purples Reds
        BrBG BuGn BuPu GnBu OrRd PRGn PiYG PuBu PuBuGn PuOr PuRd
        RdBu RdPu RdGy RdYlBu RdYlGn YlGn YlGnBu YlOrBr YlOrRd
        Spectral].each do |native_category|
-      eval <<-EOF
-        def interpolate_#{native_category.underscore}(k = nil)
-          if k
-            `window.d3.interpolate#{native_category}(k)`
-          else
-            `window.d3.interpolateRainbow`
-          end
+      define_method("interpolate_#{native_category.underscore}") do |k|
+        `window.d3`.JS["interpolate#{native_category}"].JS.apply(`window.d3`, `Opal.to_a(k)`)
+      end
+      define_method("scheme_#{native_category.underscore}") do |k=nil|
+        if k
+          `window.d3`.JS["scheme#{native_category}"][k]
+        else
+          `window.d3`.JS["scheme#{native_category}"]
         end
-        def scheme_#{native_category.underscore}(k)
-          `window.d3.scheme#{native_category}[k]`
-        end
-      EOF
+      end
     end
 
     %w[Cool CubehelixDefault Inferno Magma Plasma Rainbow Viridis Warm].each do |native_category|
-      eval <<-EOF
-        def interpolate_#{native_category.underscore}(k)
-          if k
-            `window.d3.interpolate#{native_category}(k)`
-          else
-            `window.d3.interpolateRainbow`
-          end
-        end
-      EOF
+      define_method("interpolate_#{native_category.underscore}") do |k|
+        `window.d3`.JS["interpolate#{native_category}"].JS.apply(`window.d3`, `Opal.to_a(k)`)
+      end
     end
   end
 end
